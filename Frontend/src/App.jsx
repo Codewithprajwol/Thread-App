f7d9d19678ab0e190040da090e55bf2d7da59d1b
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-02-28 23:10:37 +0545

    feat: implement dark mode feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header"
 
 function App() {
   return (
-    <div className="max-w-[600px] mx-auto py-2 bg-pink-300">
+    <div className="max-w-[600px] mx-auto py-2 bg-pink-900">
       <Header />
     </div>
   )
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DarkMode from '/DarkLogo.svg'
+import { useTheme } from './ThemeProvider'
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState("light")
+  const { setTheme } = useTheme()
+
+  const toggleDarkMode = () => {
+    if (darkMode === "light") {
+      setDarkMode("dark")
+      setTheme("dark")
+    } else {
+      setDarkMode("light")
+      setTheme("light")
+    }
+  }
   return (
     <div className='w-full flex items-center justify-center cursor-pointer'>
-        <img src={DarkMode} alt='Dark Mode' className='w-10 h-10' />
+        <img onClick={toggleDarkMode} src={DarkMode} alt='Dark Mode' className='w-7 h-7' />
     </div>
   )
 }
