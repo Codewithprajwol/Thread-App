1f45dde8909ffb843487d13287029a6692e98dbd
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-04 09:42:31 +0545

    wip: basic ui design for userpage and postpage created

--- Frontend/src/components/Comments.jsx
+++ Frontend/src/components/Comments.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsThreeDots } from 'react-icons/bs'
+import Action from './Action'
 
-const Comments = () => {
+const Comments = ({comment,createdAt,userAvatar,userName,likes}) => {
+    const [liked,setLiked]=useState(false)
   return (
-    <div>Comments</div>
+    <div className='w-full mt-4 flex items-start gap-3 justfiy-between p-4 rounded-md border-2 border-gray-700/40'>
+        <div className='flex flex-col gap-2 items-center justify-start'>
+            <div className='flex items-center justify-between w-8 h-8 rounded-full overflow-hidden'>
+                <img src={userAvatar} alt="zuckerberg" className='w-full h-full object-cover' />
+                </div>
+        </div>
+        <div to="/prajwol/post/1" className='flex flex-1 flex-col gap-1'>
+            <div className='flex  items-center justify-between'>
+               <div className="flex items-center gap-2">
+                <h6 className='font-bold'>{userName}</h6>
+                <div className='w-7 h-7'><img src="/verified.png" alt="verified logo" /></div>
+               </div>
+               <div className="flex items-center gap-2">
+                <h6 className='font-bold text-[.8rem] text-[#dadada]'>{createdAt}</h6>
+                <BsThreeDots />
+               </div>
+                
+            </div>
+            <p className='text-[0.8rem]'>{comment}</p>
+        <Action liked={liked} setLiked={setLiked}/>
+            <p>{likes+(liked?1:0)} likes</p>
+        </div>                                                                 
+        
+    </div>
   )
 }
 
--- Frontend/src/pages/PostPage.jsx
+++ Frontend/src/pages/PostPage.jsx
@@ -1,57 +1,78 @@
-import Action from '@/components/Action'
-import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
-import { BsThreeDots } from 'react-icons/bs'
-import { Separator } from "@/components/ui/separator"
+import Action from "@/components/Action";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { Separator } from "@/components/ui/separator";
+import Comments from "@/components/Comments";
 
 const PostPage = () => {
-  const [liked,setLiked]=useState(false)
+  const [liked, setLiked] = useState(false);
   return (
-    <div className='w-full'>
-         <div className='flex flex-col gap-2 items-start justify-center'>
-         <div className='flex w-full items-center justify-between'>
-                        <div className="flex items-center gap-2">
-                        <div className='flex items-center justify-between w-8 h-8 rounded-full overflow-hidden'>
-                <img src="/zuck-avatar.png" alt="zuckerberg" className='w-full h-full object-cover' />
-                </div>
-                         <h6 className='font-bold'>markzukerberg</h6>
-                         <div className='w-7 h-7'><img src="/verified.png" alt="verified logo" /></div>
-                        </div>
-                        <div className="flex items-center gap-2">
-                         <h6 className='font-bold text-[.8rem] text-[#dadada]'>1d</h6>
-                         <BsThreeDots />
-                        </div>
-                         
-         </div>
-         <p className='text-[0.8rem]'>let talk about threads.</p>
-            <div className='w-full max-h-auto overflow-hidden rounded-sm'>
-            <img src={"/post1.png"} alt="post" />
+    <div className="w-full">
+      <div className="flex flex-col gap-2 items-start justify-center">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center gap-2">
+            <div className="flex items-center justify-between w-8 h-8 rounded-full overflow-hidden">
+              <img
+                src="/zuck-avatar.png"
+                alt="zuckerberg"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <h6 className="font-bold">markzukerberg</h6>
+            <div className="w-7 h-7">
+              <img src="/verified.png" alt="verified logo" />
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <h6 className="font-bold text-[.8rem] text-[#dadada]">1d</h6>
+            <BsThreeDots />
+          </div>
         </div>
-        <Action liked={liked} setLiked={setLiked}/>
-        <div className='flex items-center justify-start pl-2 gap-2'>
-            <p>{599+(liked?1:0)} likes</p>
-            <div className='w-0.5 h-0.5 rounded-full bg-hello'></div>
-            <p>{400} replies</p>
+        <p className="text-[0.8rem]">let talk about threads.</p>
+        <div className="w-full max-h-auto overflow-hidden rounded-sm">
+          <img src={"/post1.png"} alt="post" />
+        </div>
+        <Action liked={liked} setLiked={setLiked} />
+        <div className="flex items-center justify-start pl-2 gap-2">
+          <p>{599 + (liked ? 1 : 0)} likes</p>
+          <div className="w-0.5 h-0.5 rounded-full bg-hello"></div>
+          <p>{400} replies</p>
         </div>
         {/* seperator */}
         <Separator className="my-4" />
 
-          {/* get app */}
-          <div className="w-full flex items-center justify-between">
-             <div className="flex items-center justify-start gap-2">
-              <h6>👏</h6>
-              <p className='text-gray-600'>Get the app to like, reply and post.</p>
-             </div>
-            <Button>Get</Button>
+        {/* get app */}
+        <div className="w-full flex items-center justify-between">
+          <div className="flex items-center justify-start gap-2">
+            <h6>👏</h6>
+            <p className="text-gray-600 text-sm">
+              Get the app to like, reply and post.
+            </p>
           </div>
-            {/* seperator */}
+          <Button className="cursor-pointer">Get</Button>
+        </div>
+        {/* seperator */}
         <Separator className="my-4" />
 
         {/* comment Section */}
-        
-         </div>
+        <Comments
+          comment="looks really amazing"
+          createdAt="2d"
+          likes={201}
+          userName={"krishna"}
+          userAvatar="https://avatars.githubusercontent.com/u/583231?v=4
+"/>
+        <Comments
+          comment="looks really amazing"
+          createdAt="2d"
+          likes={201}
+          userName={"krishna"}
+          userAvatar="https://avatars.githubusercontent.com/u/583231?v=4
+"/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
