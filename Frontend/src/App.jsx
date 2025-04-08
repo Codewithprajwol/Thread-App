490e6a2b8dae4c0f498c7eb4dcbd6cc470918d6f
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-04-08 10:14:21 +0545

    wip:signup component created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
 import { Toaster } from "@/components/ui/sonner"
+import AuthPage from "./pages/AuthPage"
 
 function App() {
   return (
     <div className="max-w-[600px] mx-auto py-4 px-2">
       <Header />
       <Routes>
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:id" element={<PostPage/>} />
       </Routes>
