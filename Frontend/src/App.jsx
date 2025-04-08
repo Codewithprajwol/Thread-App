09de15ca998f9887e94ae0e16c8634e9fc102ae9
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-04-08 16:46:42 +0545

    wip: created email verification page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
 import { Toaster } from "@/components/ui/sonner"
 import AuthPage from "./pages/AuthPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:id" element={<PostPage/>} />
+        <Route path="/verify-email" element={<EmailVerificationPage/>}/>
       </Routes>
       <Toaster/>
     </div>
